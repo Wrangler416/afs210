5739f4a4781a20c1ea4c77600c724de2ecb68821
kara temple <you@example.com>
2021-08-21 16:21:54 -0400

    doublelinkedlist

--- week2/double_linked/doubleLinked.py
+++ week2/double_linked/doubleLinked.py
@@ -58,33 +58,47 @@ class DoublyLinkedList:
             self.head = new_node
             self.tail = new_node
         else:
-            new_node = Node(data)
             self.head.prev = new_node
             new_node.next = self.head
             self.head = new_node
-            new_node.prev = None
+
 
 # Add a node at the end of the list
 
     def addAtEnd(self, data):
         newNode = Node(data)
-        if not self.head:
+        if not self.tail:
            self.head = newNode
            self.tail = newNode
         else: 
             self.tail.next = newNode
+            newNode.prev = self.tail
             self.tail = newNode
         self.count += 1
 
 
     def addAtIndex(self, data, index):
-        start = self.head
-        for i in range(self.count):
-           if(start.data == data):
-            return i 
-           start = start.next
-        return None
-
+        new_node = Node(data)
+        if(index < 1):
+            print("index needs to be greater than 1")
+        elif (index == 1):
+            self.head.prev = new_node
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            for i in range(1, index-1):
+                if(current != None):
+                    current = current.next
+            if(current != None):
+                new_node.next = current.next
+                new_node.prev = current
+                current.next = new_node
+                if(new_node.next != None):
+                    new_node.next.prev = new_node
+            else: 
+                print("error")
+            
         # Add a node to the list at the given index position
         # If index equals to the length of linked list, the node will be appended to the end of linked list
         # If index is greater than the length, the data will not be inserted.
@@ -101,7 +115,7 @@ class DoublyLinkedList:
         while current is not None:
             if current.data == value:
                 return position
-
+             
             current = current.next
             position += 1
 
@@ -117,30 +131,6 @@ class DoublyLinkedList:
         self.head = None
         self.tail = None
         self.count = 0
-
-    def addAtIndex(self, newElement, position):
-        newNode = Node(newElement)
-        if(position < 1):
-            print("position needs to be greater than 1")
-        elif (position == 1):
-
-            newNode.next = self.head
-            self.head.prev = newNode
-            self.head = newNode
-        else:
-            temp = self.head
-            for i in range(1, position-1):
-                if(temp != None):
-                    temp = temp.next
-        if(temp !=None):
-            newNode.next = temp.next
-            newNode.prev = temp
-            temp.next = newNode
-
-            if(newNode.next != None):
-                newNode.next.prev = newNode
-        else: 
-            print("previous node is null")
 
 
 # Delete the node at the index-th in the linked list, if the index is valid.
@@ -231,9 +221,13 @@ list.append("you")
 list.append("!")
 
 
-list.indexOf("with")
 
 
 print(list)
+print(list.indexOf("with"))
 
+list.addAtIndex("us", 5)
+print(list)
 
+list.addAtIndex("all", 6)
+print(list)
