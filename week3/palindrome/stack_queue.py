b8130f0f9e8a3c7ff534ccf7962f9c5a740db197
kara temple <you@example.com>
2021-08-26 10:33:05 -0400

    palindometake2

--- week3/palindrome/stack_queue.py
+++ week3/palindrome/stack_queue.py
@@ -41,19 +41,15 @@ class Queue:
 myQueue = Queue()
 myStack = Stack()
 
-s = "racecar"
+def isPalindrome(word):
+    if len(word) < 2: return True
+    if word[0] != word[-1]: return False
+    return isPalindrome(word[1:-1])
 
-def isPalindrome(s):
-    for i in s:
-        myStack.push(i)
-            
-        reversed = ""
-        while not myStack.isEmpty():
-         reversed += myStack.pop()
-            
-        if s == reversed:
-          print("true")
-        else: 
-          print("false")
+    return True
 
+print(isPalindrome("racecar"))
+print(isPalindrome("noon"))
+print(isPalindrome("python"))
+print(isPalindrome("madam"))
 
