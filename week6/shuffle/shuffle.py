e7b8cf706f425696e89d7fb876bcfa364e34ef7f
kara temple <you@example.com>
2021-09-08 15:38:26 -0400

    shufflealgoupdate

--- week6/shuffle/shuffle.py
+++ week6/shuffle/shuffle.py
@@ -2,7 +2,8 @@ list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 n = len(list)
 # I used the fisher yates algo which is O(n) time complexity
 # this algo starts from the last list item and swaps it with
-# a randomly assigned index position 
+# a randomly assigned index position and keeps doing so until all items are swapped
+# it is O(n) because our random number generator generates in O(1) time it is linear time
 import random
 
 def shuffle(list, n):
